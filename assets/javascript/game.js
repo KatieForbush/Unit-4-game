78ff4907ae408d2b3e56a7d72a83855fc78d7307
Kate <you@example.com>
2019-03-21 22:11:58 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,19 @@
-let randomNumbner;
+let randomNumber;
 let displayCharacters = [];
 let loses = [];
 let minNum = 1
 let maxNum = 10
 let cry1 = Math.floor(Math.random() * 10) + 1;
+$("#cry1").attr("value", cry1)
 let cry2 = Math.floor(Math.random() * 10) + 1;
+$("#cry2").attr("value", cry2)
 let cry3 = Math.floor(Math.random() * 10) + 1;
+$("#cry3").attr("value", cry3)
 let cry4 = Math.floor(Math.random() * 10) + 1;
+$("#cry4").attr("value", cry4)
 let userScore = 0;
+let losses = 0;
+let wins = 0;
 
 
 
@@ -17,44 +23,44 @@ let userScore = 0;
 //user total
 // 
 $(document).ready(function () {
+    $("#winsCount").text(wins);
+    $("#lossesCount").text(losses);
+    $("#userScore").text(userScore);
     let beginNumberMin = 19;
     let beginNumberMax = 120;
     randomNumberFromRange(beginNumberMin, beginNumberMax);
     function randomNumberFromRange(min, max) {
-        randomNumbner = (Math.floor(Math.random() * (max - min + 1) + min));
-        $("#display")
-            .text(randomNumbner)
+        randomNumber = (Math.floor(Math.random() * (max - min + 1) + min));
+        console.log(randomNumber)
+        $("#randomNumber").text(randomNumber);
+
     }
-    $('#user').text(userScore)
-
-
 
     $(".crystals").on("click", function () {
-        var crystal = $(this).attr("data-crystal");
-        if (crystal === "crystal1") {
-            userScore = userScore + cry1
-            $('#user').text(userScore)
-        } else if (crystal === "crystal2") {
-            userScore = userScore + cry2
-            $('#user').text(userScore)
-        } else if (crystal === "crystal3") {
-            userScore = userScore + cry3
-            $('#user').text(userScore)
-        } else (crystal === "crystal4"); {
-            userScore = userScore + cry4
-            $('#user').text(userScore)
-        }
-        console.log(userScore);
-        if (userScore === randomNumbner){
-        wins++;
-        $("#Wins").text(Wins)
+        var crystal = parseInt($(this).attr("value"));
+        userScore = userScore + crystal;
+        $("#userScore").text(userScore);
+        
+        if (userScore === randomNumber){
+            wins++;
+            $("#WinsCount").text(wins);
+            randomNumberFromRange(beginNumberMin, beginNumberMax);
+            userScore = 0;
+            $("#userScore").text(userScore);
+        } else if(userScore > randomNumber){
+        losses++;
+        $("#lossesCount").text(losses);
+        randomNumberFromRange(beginNumberMin, beginNumberMax);
+        userScore = 0
+        $("#userScore").text(userScore);
         }
     })
+
 
      
     })
 
-   // $(".Wins").update(userScore === display); {
-   //     alert(winner)
 
-   // }
+    //make the value assignments for the crystals into a function.
+    //put that function, as well as the randomNumberFromRange, userscore == 0, and jQuery user score into a function called reset.
+    //use the reset function at the start (document.onload), when you win, and when you lose.
